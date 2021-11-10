a3a61d877caa98b86b8686a58ce8712bb2ddc2b5
mbish08 <mdrew8@gmail.com>
2021-11-10 12:35:18 -0800

    added method to delete all completed items and redirect to index with confirmation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
 
     def remove_all_completed
-        
+        @remove_all_completed = List.where(:completed => true).destroy_all
+        redirect_to lists_url, notice: 'All completed items were successfully removed.'
     end
 
 end
